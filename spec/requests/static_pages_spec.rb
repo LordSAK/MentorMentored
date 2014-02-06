70f62e916c112556f366b351e237f417cd337813
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-02-06 00:49:31 -0800

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Sign up'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Sign Up')
     end
 
     it "should have the title 'Mentor Mentored'" do
