20f348e13c245baba81e2ee895d44fbd1ae15e81
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-02-06 04:09:52 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MentorMentored::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+  match '/signup', to: 'users#new', via: 'get'
+  root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,14 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before {visit root_path}
 
-    it "should have the content 'Sign up'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sign Up')
-    end
+    it { should have_content ('Sign up') }
 
-    it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Mentor Mentored")
-    end	
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end 
+    it { should have_title("Mentor Mentored") }
+    it { should_not have_title('| Home') }
   end
 end
