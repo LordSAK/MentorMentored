4250664b193360377401c58f60592e2bdef2b028
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-04-08 15:52:27 +0500

    Fixed queries in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def create
        
-     @userrequest = Userrequest.new( :RequestFrom => current_user.id.to_s, :RequestTo => params[:id] , :IsApproved=> "0", :SkillType => "" )
+     @userrequest = Userrequest.new( :RequestFrom => current_user.id, :RequestTo => params[:id] , :IsApproved=> "0", :SkillType => "" )
      @userrequest.save
 
      redirect_to '/searching'
@@ -20,40 +20,40 @@ class SearchController < ApplicationController
        @user = User.find(:all,:conditions=>['"user_Need" = ? ',params[:Interest]])   
     #designation
     elsif (params[:tf_Zip] == "") and ( !params[:Interest].blank? or params[:Interest] == "1" ) and ( !params[:tf_Designation].blank? or params[:tf_Designation] !="" ) and (params[:tf_Company] =="")
-       @user_join = Profession.joins(:user).where('"professions.Designation"' => params[:tf_Designation])
+       @user_join = Profession.joins(:user).where('"professions"."Designation"' => params[:tf_Designation])
     #company
     elsif (params[:tf_Zip] == "") and ( !params[:Interest].blank? or params[:Interest] == "1" ) and (params[:tf_Designation] == "") and ( !params[:tf_Company].blank? or params[:tf_Company] !="" )
-       @user_join = Profession.joins(:user).where('"professions.Company"' => params[:tf_Company])
+       @user_join = Profession.joins(:user).where('"professions"."Company"' => params[:tf_Company])
     #ZipInterest
     elsif ( !params[:tf_Zip].blank? or params[:tf_Zip] !="" )  and ( !params[:Interest].blank? or params[:Interest] !="1" ) and (params[:tf_Designation] == "") and (params[:tf_Company] == "")
        @user = User.find(:all, :conditions => ['"user_Zip" = ? AND "user_Need" = ? ',params[:tf_Zip], params[:Interest]])
     #ZipDesignation
     elsif ( !params[:tf_Zip].blank? or params[:tf_Zip] !="" ) and ( !params[:Interest].blank? and  params[:Interest] == "1" ) and ( !params[:tf_Designation].blank? or params[:tf_Designation] !="" ) and (params[:tf_Company] == "")
-        @user_join = Profession.joins(:user).where('"users.user_Zip" = ? AND "professions.Designation"= ?',params[:tf_Zip],params[:tf_Designation])
+        @user_join = Profession.joins(:user).where('"users"."user_Zip" = ? AND "professions"."Designation"= ?',params[:tf_Zip],params[:tf_Designation])
     #ZipCompany
     elsif ( !params[:tf_Zip].blank? or params[:tf_Zip] !="" )  and ( !params[:Interest].blank? and  params[:Interest] == "1" ) and (params[:tf_Designation] == "")  and ( !params[:tf_Company].blank? or params[:tf_Company] !="" )
-        @user_join = Profession.joins(:user).where('"users.user_Zip" = ? AND "professions.Company" = ?', params[:tf_Zip], params[:tf_Company])   
+        @user_join = Profession.joins(:user).where('"users"."user_Zip" = ? AND "professions"."Company" = ?', params[:tf_Zip], params[:tf_Company])   
     #InterestDesignation
     elsif (params[:tf_Zip] =="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( !params[:tf_Designation].blank? or params[:tf_Designation] !="" ) and ( params[:tf_Company] == "" )
-        @user_join = Profession.joins(:user).where('"users.user_Need" = ? AND "professions.Designation"= ?',params[:Interest],params[:tf_Designation])
+        @user_join = Profession.joins(:user).where('"users"."user_Need" = ? AND "professions"."Designation"= ?',params[:Interest],params[:tf_Designation])
     #InterestCompany
     elsif (params[:tf_Zip] =="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( params[:tf_Designation] =="" ) and ( params[:tf_Company] != "" )
-        @user_join = Profession.joins(:user).where('"users.user_Need" = ? AND "professions.Company"= ?',params[:Interest],params[:tf_Company])
+        @user_join = Profession.joins(:user).where('"users"."user_Need" = ? AND "professions"."Company"= ?',params[:Interest],params[:tf_Company])
     #DesignatinCompany
     elsif (params[:tf_Zip] =="" ) and ( !params[:Interest].blank? or params[:Interest] =="1" )  and ( params[:tf_Designation] !="" ) and ( params[:tf_Company] != "" )
-        @user_join = Profession.joins(:user).where('"professions.Designation" = ? AND "professions.Company"= ?',params[:tf_Designation], params[:tf_Company])
+        @user_join = Profession.joins(:user).where('"professions"."Designation" = ? AND "professions"."Company"= ?',params[:tf_Designation], params[:tf_Company])
     #ZipInterestDesignation
     elsif (params[:tf_Zip] !="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( params[:tf_Designation] !="" ) and ( params[:tf_Company] == "" )
-        @user_join = Profession.joins(:user).where('"users.user_Zip" = ? AND "users.user_Need" = ? AND "professions.Designation"= ?',params[:tf_Zip], params[:Interest], params[:tf_Designation])
+        @user_join = Profession.joins(:user).where('"users"."user_Zip" = ? AND "users"."user_Need" = ? AND "professions"."Designation"= ?',params[:tf_Zip], params[:Interest], params[:tf_Designation])
     #ZipInterestCompany    
     elsif (params[:tf_Zip] !="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( params[:tf_Designation] =="" ) and ( params[:tf_Company] != "" )
-        @user_join = Profession.joins(:user).where('"users.user_Zip" = ? AND "users.user_Need" = ? AND "professions.Company"= ?',params[:tf_Zip], params[:Interest], params[:tf_Company])
+        @user_join = Profession.joins(:user).where('"users"."user_Zip" = ? AND "users"."user_Need" = ? AND "professions"."Company"= ?',params[:tf_Zip], params[:Interest], params[:tf_Company])
     #InterestDesignationCompany
     elsif (params[:tf_Zip] =="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( params[:tf_Designation] !="" ) and ( params[:tf_Company] != "" )
-        @user_join = Profession.joins(:user).where('"users.user_Need" = ? AND "professions.Designation"= ? AND "professions.Company" = ?', params[:Interest], params[:tf_Designation], params[:tf_Company])
+        @user_join = Profession.joins(:user).where('"users"."user_Need" = ? AND "professions"."Designation"= ? AND "professions"."Company" = ?', params[:Interest], params[:tf_Designation], params[:tf_Company])
    
     else (params[:tf_Zip] !="" ) and ( !params[:Interest].blank? or params[:Interest] !="1" )  and ( params[:tf_Designation] !="" ) and ( params[:tf_Company] != "" )
-        @user_join = Profession.joins(:user).where('"users.user_Zip" = ? AND "users.user_Need" = ? AND "professions.Designation"= ? AND "professions.Company"= ?',params[:tf_Zip], params[:Interest], params[:tf_Designation], params[:tf_Company])
+        @user_join = Profession.joins(:user).where('"users"."user_Zip" = ? AND "users"."user_Need" = ? AND "professions"."Designation"= ? AND "professions"."Company"= ?',params[:tf_Zip], params[:Interest], params[:tf_Designation], params[:tf_Company])
     
        
   	end   	
