7ff649a7517aa9bbb85ee1a25558e167930eb095
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-04-08 15:26:16 +0500

    fixed issue in cast of migration

--- db/migrate/20140408101222_change_professions_table.rb
+++ db/migrate/20140408101222_change_professions_table.rb
@@ -1,6 +1,6 @@
 class ChangeProfessionsTable < ActiveRecord::Migration
   def change
-  	change_column :professions,:UserID, :integer
-  	change_column :educations,:UserID,:integer
+  	change_column :professions,:UserID, 'integer USING CAST(UserID AS integer)'
+  	change_column :educations,:UserID,'integer USING CAST(UserID AS integer)'
   end
 end
